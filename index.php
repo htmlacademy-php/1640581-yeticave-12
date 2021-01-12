62dd78f5cd094d734e0e9cb311b526c3e5941837
Динар <dinars-1995@mail.ru>
2021-01-12 22:40:29 +0300

    1 modul, 4 task
    добавил условие

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $user_name = 'Динар'; // укажите здесь ваше имя
 <head>
     <meta charset="UTF-8">
     <title>Главная</title>
-    <link href="../css/normalize.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="../1640581-yeticave-12/css/normalize.min.css" rel="stylesheet">
+    <link href="../1640581-yeticave-12/css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="page-wrapper">
@@ -18,7 +18,7 @@ $user_name = 'Динар'; // укажите здесь ваше имя
     <div class="main-header__container container">
         <h1 class="visually-hidden">YetiCave</h1>
         <a class="main-header__logo">
-            <img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
+            <img src="../1640581-yeticave-12/img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
         </a>
         <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
             <input type="search" name="search" placeholder="Поиск лота">
@@ -28,6 +28,29 @@ $user_name = 'Динар'; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
+            <?php
+
+            if ($is_auth == 1) {
+
+                echo
+                "<div class=\"user-menu__logged\">
+                   <p>Динар</p>
+                   <a class=\"user-menu__bets\" href=\"pages/my-bets.html\">Мои ставки</a>
+                   <a class=\"user-menu__logout\" href=\"#\">Выход</a>
+                </div>";
+            }
+            else {
+                echo
+                "<ul class=\"user-menu__list\">
+                   <li class=\"user-menu__item\">
+                     <a href=\"#\">Регистрация</a>
+                   </li>
+                   <li class=\"user-menu__item\">
+                     <a href=\"#\">Вход</a>
+                   </li>
+                 </ul>";
+            }
+            ?>
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
         </nav>
